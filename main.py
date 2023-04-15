c065bb11c0e5b59be5cce7dc4e75f4de3f2f4148
Matthew Thompson <thompsonson@gmail.com>
2023-04-15 17:15:48 +0000

    fix: running the telegram bot at the same time

--- main.py
+++ main.py
@@ -23,24 +23,23 @@ async def root():
 
 
 # logic for below from: https://github.com/LonamiWebs/Telethon/blob/2a7d4317bd20c171a36abe93aac417a460643c99/telethon_examples/quart_login.py
-# bot = TelethonWrapper()  # type: ignore
+bot = TelethonWrapper()  # type: ignore
 
-# bot.add_event_handler(correct_handler, events.NewMessage(pattern="/correct"))
-# bot.add_event_handler(translate_handler, events.NewMessage(pattern="/translate"))
-# bot.add_event_handler(enrich_handler, events.NewMessage(pattern="/enrich"))
+bot.add_event_handler(correct_handler, events.NewMessage(pattern="/correct"))
+bot.add_event_handler(translate_handler, events.NewMessage(pattern="/translate"))
+bot.add_event_handler(enrich_handler, events.NewMessage(pattern="/enrich"))
 
-# bot.add_event_handler(correct_handler, events.CallbackQuery(data=b"correct"))
-# bot.add_event_handler(translate_handler, events.CallbackQuery(data=b"translate"))
-# bot.add_event_handler(enrich_handler, events.CallbackQuery(data=b"enrich"))
-# bot.add_event_handler(corriger_handler, events.CallbackQuery(data=b"corriger"))
+bot.add_event_handler(correct_handler, events.CallbackQuery(data=b"correct"))
+bot.add_event_handler(translate_handler, events.CallbackQuery(data=b"translate"))
+bot.add_event_handler(enrich_handler, events.CallbackQuery(data=b"enrich"))
+bot.add_event_handler(corriger_handler, events.CallbackQuery(data=b"corriger"))
 
-# bot.add_event_handler(new_message_handler, events.NewMessage(outgoing=False))
-
-# bot.start_bot()  # type: ignore
-
-# app.run(
-#    loop=bot.get_loop(), host="0.0.0.0", port=8080
-# )  # <- same event loop as telethon
+bot.add_event_handler(new_message_handler, events.NewMessage(outgoing=False))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    bot.start_bot()  # type: ignore
+
+app.run(
+    loop=bot.get_loop(), host="0.0.0.0", port=8080
+)  # <- same event loop as telethon
+#    app.run(host="0.0.0.0", port=8080)
